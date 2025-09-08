edbeb399aeb78627b126faf684758953c0371a4c
M.Usaid Saddiq <168702178+Usaid136@users.noreply.github.com>
2025-09-07 23:08:59 -0700

    Update and rename medicines.sql to medical_store_db.sql

--- medical_store_db.sql
+++ medical_store_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2025 at 08:46 AM
+-- Generation Time: Sep 08, 2025 at 08:04 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `medical_store_db`
 --
+CREATE DATABASE IF NOT EXISTS `medical_store_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `medical_store_db`;
 
 -- --------------------------------------------------------
 
